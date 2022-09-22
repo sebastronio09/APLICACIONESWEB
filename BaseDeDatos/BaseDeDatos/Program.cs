7d6994a52b31b086261741e27367c284c3809eff
sebastronio09 <andersmaria61@gmail.com>
2022-09-22 01:50:45 -0700

    practica 5

---
+++ BaseDeDatos/BaseDeDatos/Program.cs
@@ -0,0 +1,42 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+
+
+try
+{
+    MySqlConnectionStringBuilder builder = new MySqlConnectionStringBuilder()
+    {
+        Server = "localhost",
+        Database = "empresa_1170441",
+        UserID = "root",
+        Password = "sebastronio09",
+        Port = 3306,
+    };
+    Console.WriteLine(builder.ConnectionString);
+    using (MySqlConnection connection = new MySqlConnection(builder.ConnectionString))
+    {
+        Console.WriteLine("\nQuery data example:");
+        Console.WriteLine("=========================================\n");
+
+        connection.Open();
+
+        String sql = "SELECT nombre FROM empresa_1170441.empleado";
+
+        using (MySqlCommand command = new MySqlCommand(sql, connection))
+        {
+            using (MySqlDataReader reader = command.ExecuteReader())
+            {
+                while (reader.Read())
+                {
+                    Console.WriteLine("{0}", reader.GetString(0));
+                }
+            }
+        }
+    }
+}
+catch (MySqlException e)
+{
+    Console.WriteLine(e.ToString());
+}
+Console.WriteLine("\nDone. Press enter.");
+Console.ReadLine();
